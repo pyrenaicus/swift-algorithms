865346bdee766d30546fcd3c985dbe05dffd4006
Pau Guerrero Prado <pauguerrero@gmail.com>
2025-07-24 13:45:44 +0200

    linear search

--- linear-search/Sources/main.swift
+++ linear-search/Sources/main.swift
@@ -1,4 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+// Linear Search Implementation in Swift
 
-print("Hello, world!")
+
+func linearSearch(arr: [Int], num: Int) -> Bool {
+    for i in 0..<arr.count {
+        if arr[i] == num {
+            return true
+        }
+    }
+    return false
+}
+
+
+let numbers = [1, 2, 3, 4, 5]
+let searchNumber = 3
+
+let found = linearSearch(arr: numbers, num: searchNumber)
+print("Number \(searchNumber) found: \(found)")
